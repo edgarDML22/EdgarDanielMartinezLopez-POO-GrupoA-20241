dc3ca67133ac59ed1fd5b4cfd79d89bb93c4796f
Edgar Martínez <edgar.ml.2005@outlook.com>
2024-02-22 11:18:39 -0600

    Cambios de clase 22/08/2024

---
+++ unidad2/notas.java
@@ -0,0 +1,19 @@
+public class notas {
+    public static void main(String[] args) {
+        
+        /*
+        Método String.format: 
+        String cadena = String.format("Se mostrará el primer parámetro %s, 
+        se mostrará el segundo parámetro %f", paramétro1, parámetro2);
+
+        %d - int
+        %s - String
+        %f - double
+
+        System.out.println(String.format("IMPRESION")) = System.out.printf(IMPRESION);
+         */
+        
+    }
+
+    
+}
