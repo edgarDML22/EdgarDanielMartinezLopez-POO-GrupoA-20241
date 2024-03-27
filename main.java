6de1c9d38fe9644051153d54cb877c0bf10eec40
CokeMat <159500799+CokeMat@users.noreply.github.com>
2024-03-27 11:51:28 -0600

    Cambios realizados

--- main.java
+++ main.java
@@ -3,7 +3,8 @@ public class main {
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO :)))");
         System.out.println("develop");
-        
+        System.out.println("ADIOS");
+        System.out.println("Saquen fortnite");
     }
     
 }
